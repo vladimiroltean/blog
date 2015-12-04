02b94a6eb4a98e8dca9ee21a82a10b95fb9406c1
Vladimir Oltean <olteanv@gmail.com>
2015-12-04 11:00:59 +0200

    Added backup ~/.ssh.old and ~/.dotfiles.old folder generation

--- install.sh
+++ install.sh
@@ -1,20 +1,22 @@
 #/bin/bash
 
-mv ~/.bashrc.after        ~/.bashrc.after.old
-mv ~/.bash_aliases        ~/.bash_aliases.old
-mv ~/.gvimrc.after        ~/.gvimrc.after.old
-mv ~/.vimrc.after         ~/.vimrc.after.old
-mv ~/.vimrc.autocomplete  ~/.vimrc.autocomplete.old
-mv ~/.vimrc.basic         ~/.vimrc.basic.old
-mv ~/.vimrc.keymaps       ~/.vimrc.keymaps.old
-mv ~/.vimrc.syntastic     ~/.vimrc.syntastic.old
-mv ~/.vimrc.tags          ~/.vimrc.tags.old
-mv ~/.vimpagerrc          ~/.vimpagerrc.old
-mv ~/.tmux.conf           ~/.tmux.conf.old
-mv ~/.tmux.compat.conf    ~/.tmux.compat.conf.old
-mv ~/.tmux.keys.conf      ~/.tmux.keys.conf.old
-mv ~/.tmux.mouse.conf     ~/.tmux.mouse.conf.old
-mv ~/.tmux.visual.conf    ~/.tmux.visual.conf.old
+mkdir -p ~/.dotfiles.old
+
+mv ~/.bashrc.after        ~/.dotfiles.old
+mv ~/.bash_aliases        ~/.dotfiles.old
+mv ~/.gvimrc.after        ~/.dotfiles.old
+mv ~/.vimrc.after         ~/.dotfiles.old
+mv ~/.vimrc.autocomplete  ~/.dotfiles.old
+mv ~/.vimrc.basic         ~/.dotfiles.old
+mv ~/.vimrc.keymaps       ~/.dotfiles.old
+mv ~/.vimrc.syntastic     ~/.dotfiles.old
+mv ~/.vimrc.tags          ~/.dotfiles.old
+mv ~/.vimpagerrc          ~/.dotfiles.old
+mv ~/.tmux.conf           ~/.dotfiles.old
+mv ~/.tmux.compat.conf    ~/.dotfiles.old
+mv ~/.tmux.keys.conf      ~/.dotfiles.old
+mv ~/.tmux.mouse.conf     ~/.dotfiles.old
+mv ~/.tmux.visual.conf    ~/.dotfiles.old
 mv ~/.ssh                 ~/.ssh.old
 
 cp ./.bashrc.after        ~
@@ -34,3 +36,13 @@ cp ./.tmux.mouse.conf     ~
 cp ./.tmux.visual.conf    ~
 mkdir -p ~/.ssh/ && cp ./.ssh_config ~/.ssh/config
 rsync -avr ./bin ~
+
+echo -e \
+"Please check that everything is ok, then press any key... \n
+If there is any problem, please kill this process (Ctrl-c). You will \
+find your old files inside ~/.dotiles.old and ~/.ssh.old. \n
+If you continue, they will be deleted."
+read
+
+rm -r ~/.ssh.old
+rm -r ~/.dotfiles.old
